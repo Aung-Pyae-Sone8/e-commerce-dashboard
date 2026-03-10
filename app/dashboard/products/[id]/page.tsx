ef5d3789e75333f727bd5951cde992631c19651a
Aung Pyae Sone <aungpyaesone@Aungs-MacBook-Air.local>
2026-03-10 16:13:00 +0630

    Product details screen

---
+++ app/dashboard/products/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { fetchProductById } from "@/api/products";
+import ProductListItem from "../ProductListItem";
+
+interface Product {
+  id: string;
+}
+
+const ProductPage = async ({ params }: { params: Product }) => {
+  const product = await fetchProductById(Number(params.id));
+  return (
+    <div className="max-w-[1200px] mx-auto w-full">
+      <ProductListItem product={product} />
+    </div> 
+  );
+};
+
+export default ProductPage;
